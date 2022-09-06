f3eac6106be19e0be3de4bb8280d0853b6a2c222
bpmExpMichelFernandes <michelfernandes3@gmail.com>
2022-09-06 09:25:44 -0300

    Criado endpoint salvar para Cliente, pendente requestDTO e desenvolvimento do valid

--- gestao-vendas/gestao-vendas/src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
+++ gestao-vendas/gestao-vendas/src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +47,15 @@ public class ClienteControlador {
                 ? ResponseEntity.ok(ClienteResponseDTO.converterParaClienteResponseDTO(cliente.get()))
                 : ResponseEntity.notFound().build();
     }
+
+        // @valid irá aplicar as constrints bean validatio da class produto @NotNull e
+    // @Length
+    @ApiOperation(value = "Salvar", nickname = "salvarCliente")
+    @PostMapping
+    public ResponseEntity<ClienteResponseDTO> salvar(@Valid @RequestBody Cliente cliente) {
+        Cliente clienteSalvo = clienteServico.salvar(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ClienteResponseDTO.converterParaClienteResponseDTO(clienteSalvo));
+    }
+    
 }
--- gestao-vendas/gestao-vendas/src/main/java/com/gvendas/gestaovendas/servico/ClienteServico.java
+++ gestao-vendas/gestao-vendas/src/main/java/com/gvendas/gestaovendas/servico/ClienteServico.java
@@ -6,12 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gvendas.gestaovendas.excecao.RegraDeNegocioException;
 import com.gvendas.gestaovendas.entidades.Cliente;
 import com.gvendas.gestaovendas.repositorio.ClienteRepositorio;
 
 @Service
 public class ClienteServico {
-    
+
     @Autowired
     private ClienteRepositorio clienteRepositorio;
 
@@ -22,5 +23,17 @@ public class ClienteServico {
     public Optional<Cliente> buscarPorCodigo(Long codigo) {
         return clienteRepositorio.findById(codigo);
     }
-    
+
+    public Cliente salvar(Cliente cliente) {
+        validarClienteDuplicado(cliente);
+        return clienteRepositorio.save(cliente);
+    }
+
+    private void validarClienteDuplicado(Cliente cliente) {
+        Cliente clientePorNome = clienteRepositorio.findByNome(cliente.getNome());
+        if (clientePorNome != null && clientePorNome.getCodigo() != cliente.getCodigo()) {
+            throw new RegraDeNegocioException(
+                    String.format("O cliente %s já está cadastrado", cliente.getNome().toUpperCase()));
+        }
+    }
 }
