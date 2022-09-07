bb34cf2c5018784f509b0230368250fffac98a21
bpmExpMichelFernandes <michelfernandes3@gmail.com>
2022-09-07 15:55:58 -0300

    Mapeando entidade Venda e item venda com JPA

---
+++ gestao-vendas/gestao-vendas/src/main/java/com/gvendas/gestaovendas/entidades/Venda.java
@@ -0,0 +1,88 @@
+package com.gvendas.gestaovendas.entidades;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "venda")
+public class Venda {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private long codigo;
+
+    @Column(name = "data")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo")
+    Cliente cliente;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+        result = prime * result + (int) (codigo ^ (codigo >>> 32));
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Venda other = (Venda) obj;
+        if (cliente == null) {
+            if (other.cliente != null)
+                return false;
+        } else if (!cliente.equals(other.cliente))
+            return false;
+        if (codigo != other.codigo)
+            return false;
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
+        return true;
+    }
+
+}
